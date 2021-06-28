fcf142a3d118e28f18662cd0572364ac990bd17e
Alberto Gomez Juan <agomezjuan@hotmail.com>
2021-06-28 11:28:27 -0400

    Primer commit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    /**
+     * 
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Buenos días Java!");
+
+        // Este es mi primer programa en Java
+
+        /*
+         * Parece que programar en Java no es tan tormentoso como se cree.
+         */
+    }
+}
