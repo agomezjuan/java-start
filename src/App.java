29259d35443a95bc49a5a227fce085f496e96e32
Alberto Gomez Juan <agomezjuan@hotmail.com>
2021-07-31 18:35:49 +0200

    Probando tabla de colores

--- src/App.java
+++ src/App.java
@@ -1,18 +1,10 @@
+
 public class App {
     /**
      * 
      * @param args
      * @throws Exception
      */
-    public static void main(String[] args) throws Exception {
-        System.out.println("Buenos días Java!");
 
-        System.out.println("Ahora buenas tardes!");
-
-        // Este es mi primer programa en Java
-
-        /*
-         * Parece que programar en Java no es tan tormentoso como se cree.
-         */
-    }
+    
 }
