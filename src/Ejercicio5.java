89709680ae5c5e859f57dd1e9c906dcb2ec84965
Alberto Gomez Juan <agomezjuan@hotmail.com>
2021-08-02 22:08:22 +0200

    Solución al ejercicio 5

---
+++ src/Ejercicio5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    /**
+     * Generar un número aleatorio entre el 1 y el 100, el usuario lo tiene que
+     * adivinar introduciendo el número por teclado. En el caso que el número a
+     * adivinar sea mayor al ingresado, decirle al usuario “El número que busca es
+     * mayor”, de lo contrario, “El número que busca es menor”. El programa
+     * finalizará cuando se introduzca el número correcto. Nota: usar la clase
+     * Random para generar el número aleatorio.
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+
+    }
+}
